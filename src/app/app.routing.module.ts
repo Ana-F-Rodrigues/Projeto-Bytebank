3c8ee083afa72e5dc3c9e61f9624cf6d6a3cecff
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2021-09-03 14:06:39 -0300

    Subindo projeto

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,26 @@
+
+
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+
+
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+
+import { ExtratoComponent } from './extrato/extrato.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'extrato', pathMatch: 'full' },
+  { path: 'extrato', component: ExtratoComponent },
+  { path: 'nova-transferencia', component: NovaTransferenciaComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+
+export class AppRoutingModule { }
+
+
